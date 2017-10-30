82f6f3cfb315224e5675973e4e46ae3614f76625
Daniel Isaac Khan Ramiro <di.khan.r@gmail.com>
2017-10-29 23:25:45 -0700

    Update example with newly created service provider - cdn

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-resource "sp_users" "my_user" {
+resource "sp1_cdns" "my_cdn" {
+  label = "label"
+  ips = ["127.0.0.1"]
+  hostnames = ["origin.com"]
+}
+
+resource "sp2_users" "my_user" {
   username = "dikhan"
   first_name = "Daniel"
   last_name = "Khan"
