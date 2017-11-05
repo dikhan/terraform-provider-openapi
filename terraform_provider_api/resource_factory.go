0687e4c740886461f0461209dae6590700418cbf
Daniel Isaac Khan Ramiro <di.khan.r@gmail.com>
2017-11-04 22:25:15 -0700

    Add support for resource update

--- terraform_provider_api/resource_factory.go
+++ terraform_provider_api/resource_factory.go
@@ -73,18 +73,8 @@ func (r ResourceFactory) getType(property spec.Schema) schema.ValueType {
 }
 
 func (r ResourceFactory) create(data *schema.ResourceData, i interface{}) error {
-	input := map[string]interface{}{}
+	input := r.getPayloadFromData(data)
 	output := map[string]interface{}{}
-	for propertyName, _ := range r.ResourceInfo.SchemaDefinition.Properties {
-		if propertyName == "id" {
-			continue
-		}
-		if reflect.TypeOf(data.Get(propertyName)).Kind() == reflect.Slice {
-			input[propertyName] = data.Get(propertyName).([]interface{})
-		} else {
-			input[propertyName] = data.Get(propertyName).(string)
-		}
-	}
 	httpClient := httpGoClient.HttpClient{HttpClient: r.httpClient}
 	url := r.getResourceUrl()
 	_, err := httpClient.PostJson(url, nil, input, &output)
@@ -103,13 +93,21 @@ func (r ResourceFactory) read(data *schema.ResourceData, i interface{}) error {
 	if err != nil {
 		return err
 	}
-	for propertyName, propertyValue := range output {
-		data.Set(propertyName, propertyValue)
-	}
+	r.updateResourceState(output, data)
 	return nil
 }
 
 func (r ResourceFactory) update(data *schema.ResourceData, i interface{}) error {
+	input := r.getPayloadFromData(data)
+	output := map[string]interface{}{}
+
+	httpClient := httpGoClient.HttpClient{HttpClient: r.httpClient}
+	url := r.getResourceIdUrl(data.Id())
+	_, err := httpClient.PutJson(url, nil, input, &output)
+	if err != nil {
+		return err
+	}
+	r.updateResourceState(output, data)
 	return nil
 }
 
@@ -121,6 +119,27 @@ func (r ResourceFactory) delete(data *schema.ResourceData, i interface{}) error 
 		return err
 	}
 	return nil
+}
+
+func (r ResourceFactory) updateResourceState(input map[string]interface{}, data *schema.ResourceData) {
+	for propertyName, propertyValue := range input {
+		data.Set(propertyName, propertyValue)
+	}
+}
+
+func (r ResourceFactory) getPayloadFromData(data *schema.ResourceData) map[string]interface{} {
+	input := map[string]interface{}{}
+	for propertyName, _ := range r.ResourceInfo.SchemaDefinition.Properties {
+		if propertyName == "id" {
+			continue
+		}
+		if reflect.TypeOf(data.Get(propertyName)).Kind() == reflect.Slice {
+			input[propertyName] = data.Get(propertyName).([]interface{})
+		} else {
+			input[propertyName] = data.Get(propertyName).(string)
+		}
+	}
+	return input
 }
 
 func (r ResourceFactory) getResourceUrl() string {
