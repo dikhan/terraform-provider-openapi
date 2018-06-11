60fac9fb73cfa2ff6f79bbd068b7bcc503ce80ca
dkhanram <dkhanram@opendns.com>
2018-06-11 16:42:11 +0100

    move cdn api example to example folder
    - this follows terraform provider folder structure

---
+++ examples/cdn/main.tf
@@ -0,0 +1,15 @@
+provider "sp" {
+  ## the example server is expecting the api key to have 'apiKeyValue' (it's hard coded)
+  ## auth testing can be done by tweaking this value to be something else
+  apikey_auth = "${var.apikey_auth}"
+}
+
+resource "sp_cdns_v1" "my_cdn" {
+  label = "label" ## This is an immutable property (refer to swagger file)
+  ips = ["127.0.0.1"] ## This is a force-new property (refer to swagger file)
+  hostnames = ["origin.com"]
+
+  example_int = 12
+  example_number = 1.12
+  #example_boolean = true  this is a readOnly property - not setting a value as it's supposed to be computed by API
+}
