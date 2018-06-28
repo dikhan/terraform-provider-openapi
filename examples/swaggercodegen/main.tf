cbffce048d121f8edc4e92928e4a9ff1a0846d24
dkhanram <dkhanram@opendns.com>
2018-06-27 20:48:28 -0700

    rename cdn example folder to codegen to follow examples pattern

--- examples/swaggercodegen/main.tf
+++ examples/swaggercodegen/main.tf
@@ -1,10 +1,10 @@
-provider "sp" {
+provider "swaggercodegen" {
   ## the example server is expecting the api key to have 'apiKeyValue' (it's hard coded)
   ## auth testing can be done by tweaking this value to be something else
   apikey_auth = "${var.apikey_auth}"
 }
 
-resource "sp_cdns_v1" "my_cdn" {
+resource "swaggercodegen_cdns_v1" "my_cdn" {
   label = "label" ## This is an immutable property (refer to swagger file)
   ips = ["127.0.0.1"] ## This is a force-new property (refer to swagger file)
   hostnames = ["origin.com"]
