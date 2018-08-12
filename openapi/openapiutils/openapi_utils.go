9efe682f7ca0f2b2b5b866db58a9572dd017d4c5
Daniel Isaac Khan Ramiro <di.khan.r@gmail.com>
2018-08-12 10:50:20 -0700

    add support for undefined host in swagger doc
    - As per OpenAPI documentation, if host is not specified, it is assumed
    to be the same host where the API documentation is being served.
    
    Reference link:
    https://swagger.io/docs/specification/2-0/api-host-and-base-path/#host

--- openapi/openapiutils/openapi_utils.go
+++ openapi/openapiutils/openapi_utils.go
@@ -2,9 +2,11 @@ package openapiutils
 
 import (
 	"github.com/go-openapi/spec"
+	"regexp"
 	"strings"
 )
 
+const fqdnInURLRegex = `\b(?:(?:[^.-/]{0,1})[\w-]{1,63}[-]{0,1}[.]{1})+(?:[a-zA-Z]{2,63})?|localhost(?:[:]\d+)?\b`
 const extTfHeader = "x-terraform-header"
 
 // HeaderConfigurations defines the header configurations that on runtime will be used by the resource factory to
@@ -52,6 +54,23 @@ func GetAllHeaderParameters(spec *spec.Swagger) HeaderConfigurations {
 		}
 	}
 	return headerConfigProps
+}
+
+// GetHostFromURL returns the fqdn of a given string (localhost including port number is also handled).
+// Example domains that would match:
+// - http://domain.com/
+// - domain.com/parameter
+// - domain.com?anything
+// - example.domain.com
+// - example.domain-hyphen.com
+// - www.domain.com
+// - localhost
+// - localhost:8080
+// Example domains that would not match:
+// - http://domain.com:8080 (this use case is not support at the moment, it is assumed that actual domains will use standard ports)
+func GetHostFromURL(url string) string {
+	re := regexp.MustCompile(fqdnInURLRegex)
+	return re.FindString(url)
 }
 
 func convertToTerraformCompliantFieldName(name string) string {
