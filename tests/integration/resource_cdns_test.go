7364fd118168b18d671798df00b16c91845f6a04
Daniel Isaac Khan Ramiro <di.khan.r@gmail.com>
2018-09-09 19:51:58 -0700

    clean up int tests

--- tests/integration/resource_cdns_test.go
+++ tests/integration/resource_cdns_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/go-openapi/loads"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-	"log"
 	"net/http"
 	"path/filepath"
 	"regexp"
@@ -23,103 +22,13 @@ var openAPIResourceName = fmt.Sprintf("%s_%s", providerName, resourceName)
 var openAPIResourceInstanceName = "my_cdn"
 var openAPIResourceState = fmt.Sprintf("%s.%s", openAPIResourceName, openAPIResourceInstanceName)
 
-var cdn = newContentDeliveryNetwork("someLabel", []string{"192.168.0.2"}, []string{"www.google.com"}, 10, 12.22, true)
-var cdnUpdated = newContentDeliveryNetwork(cdn.Label, cdn.Ips, cdn.Hostnames, 14, 14.14, false)
-var cdnUpdatedForceNew = newContentDeliveryNetwork(cdn.Label, []string{"192.168.1.5"}, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean)
-
+var cdn api.ContentDeliveryNetwork
 var testCDNCreateConfig string
-var testCDNCreateWrongApiKeyAuthConfig string
-var testCDNUpdatedConfig string
-var testCDNUpdatedImmutableConfig string
-var testCDNUpdatedForceNewConfig string
 
 func init() {
-	testCDNCreateConfig = fmt.Sprintf(`provider "%s" {
-  apikey_auth = "apiKeyValue" # this is the value expected bythe API when perfoming the authentication
-  x_request_id = "some value..."
-}
-
-resource "%s" "my_cdn" {
-  label = "%s" # This is an immutable property (refer to swagger file)
-  ips = ["%s"] # This is a force-new property (refer to swagger file)
-  hostnames = ["%s"]
-
-  example_int = %d
-  better_example_number_field_name = %s
-  example_boolean = %v
-}`, providerName, openAPIResourceName, cdn.Label, arrayToString(cdn.Ips), arrayToString(cdn.Hostnames), cdn.ExampleInt, floatToString(cdn.ExampleNumber), cdn.ExampleBoolean)
-
-	testCDNCreateWrongApiKeyAuthConfig = fmt.Sprintf(`provider "%s" {
-  apikey_auth = "This is not the key expected by the API to authenticate the client"
-  x_request_id = "some value..."
-}
-
-resource "%s" "my_cdn" {
-  label = "%s" # This is an immutable property (refer to swagger file)
-  ips = ["%s"] # This is a force-new property (refer to swagger file)
-  hostnames = ["%s"]
-
-  example_int = %d
-  better_example_number_field_name = %s
-  example_boolean = %v
-}`, providerName, openAPIResourceName, cdn.Label, arrayToString(cdn.Ips), arrayToString(cdn.Hostnames), cdn.ExampleInt, floatToString(cdn.ExampleNumber), cdn.ExampleBoolean)
-
-	testCDNUpdatedConfig = fmt.Sprintf(`provider "%s" {
-  apikey_auth = "apiKeyValue" # this is the value expected bythe API when perfoming the authentication
-  x_request_id = "some value..."
-}
-
-resource "%s" "my_cdn" {
-  label = "%s" # This is an immutable property (refer to swagger file)
-  ips = ["%s"] # This is a force-new property (refer to swagger file)
-  hostnames = ["%s"]
-
-  example_int = %d
-  better_example_number_field_name = %s
-  example_boolean = %v
-}`, providerName, openAPIResourceName, cdnUpdated.Label, arrayToString(cdnUpdated.Ips), arrayToString(cdnUpdated.Hostnames), cdnUpdated.ExampleInt, floatToString(cdnUpdated.ExampleNumber), cdnUpdated.ExampleBoolean)
-
-	testCDNUpdatedImmutableConfig = fmt.Sprintf(`provider "%s" {
-  apikey_auth = "apiKeyValue" # this is the value expected bythe API when perfoming the authentication
-  x_request_id = "some value..."
-}
-
-resource "%s" "my_cdn" {
-  label = "%s" # This is an immutable property (refer to swagger file)
-  ips = ["%s"] # This is a force-new property (refer to swagger file)
-  hostnames = ["%s"]
-
-  example_int = %d
-  better_example_number_field_name = %s
-  example_boolean = %v
-}`, providerName, openAPIResourceName, "label updated", arrayToString(cdnUpdated.Ips), arrayToString(cdnUpdated.Hostnames), cdnUpdated.ExampleInt, floatToString(cdnUpdated.ExampleNumber), cdnUpdated.ExampleBoolean)
-
-	testCDNUpdatedForceNewConfig = fmt.Sprintf(`provider "%s" {
-  apikey_auth = "apiKeyValue" # this is the value expected bythe API when perfoming the authentication
-  x_request_id = "some value..."
-}
-
-resource "%s" "my_cdn" {
-  label = "%s" # This is an immutable property (refer to swagger file)
-  ips = ["%s"] # This is a force-new property (refer to swagger file)
-  hostnames = ["%s"]
-
-  example_int = %d
-  better_example_number_field_name = %s
-  example_boolean = %v
-}`, providerName, openAPIResourceName, cdnUpdatedForceNew.Label, cdnUpdatedForceNew.Ips, arrayToString(cdnUpdatedForceNew.Hostnames), cdnUpdatedForceNew.ExampleInt, floatToString(cdnUpdatedForceNew.ExampleNumber), cdnUpdatedForceNew.ExampleBoolean)
-
-}
-
-func newContentDeliveryNetwork(label string, ips, hostnames []string, exampleInt int32, exampleNumber float32, exampleBool bool) api.ContentDeliveryNetwork {
-	return api.ContentDeliveryNetwork{
-		Label:          label,
-		Ips:            ips,
-		Hostnames:      hostnames,
-		ExampleInt:     exampleInt,
-		ExampleNumber:  exampleNumber,
-		ExampleBoolean: exampleBool,
-	}
+	// Setting this up here as it is used by many different tests
+	cdn = newContentDeliveryNetwork("someLabel", []string{"192.168.0.2"}, []string{"www.google.com"}, 10, 12.22, true)
+	testCDNCreateConfig = populateTemplateConfiguration(cdn.Label, cdn.Ips, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean)
 }
 
 func TestAccCDN_Create(t *testing.T) {
@@ -154,7 +63,38 @@ func TestAccCDN_Create(t *testing.T) {
 	})
 }
 
+func TestAccCDN_CreateFailsDueToMissingMandatoryApiKeyAuth(t *testing.T) {
+	testCDNCreateMissingApiKeyAuthConfig := fmt.Sprintf(`provider "%s" {
+  # apikey_auth = "apiKeyValue" simulating configuration that is missing the mandatory apikey_auth (commented out for the reference)
+  x_request_id = "some value..."
+}
+resource "%s" "my_cdn" {}`, providerName, openAPIResourceName)
+
+	expectedValidationError, _ := regexp.Compile(".*\"apikey_auth\": required field is not set.*")
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckCDNsV1Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config:      testCDNCreateMissingApiKeyAuthConfig,
+				ExpectError: expectedValidationError,
+			},
+		},
+	})
+}
+
 func TestAccCDN_CreateFailsDueToWrongAuthKeyValue(t *testing.T) {
+	testCDNCreateWrongApiKeyAuthConfig := fmt.Sprintf(`provider "%s" {
+  apikey_auth = "This is not the key expected by the API to authenticate the client, it should be 'apiKeyValue'' :)"
+  x_request_id = "some value..."
+}
+resource "%s" "my_cdn" {
+  label = "%s"
+  ips = ["%s"]
+  hostnames = ["%s"]
+}`, providerName, openAPIResourceName, cdn.Label, arrayToString(cdn.Ips), arrayToString(cdn.Hostnames))
+
 	expectedValidationError, _ := regexp.Compile(".*{\"code\":\"401\", \"message\": \"unauthorized user\"}.*")
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -169,8 +109,34 @@ func TestAccCDN_CreateFailsDueToWrongAuthKeyValue(t *testing.T) {
 	})
 }
 
+func TestAccCDN_CreateFailsDueToRequiredPropertyMissing(t *testing.T) {
+	testCDNCreateConfigMissingRequiredProperty := fmt.Sprintf(`provider "%s" {
+  apikey_auth = "apiKeyValue"
+  x_request_id = "some value..."
+}
+resource "%s" "my_cdn" {
+  #label = "%s" # ==> Simulating required field is missing (commented out for the reference)
+  ips = ["%s"]
+  hostnames = ["%s"]
+}`, providerName, openAPIResourceName, cdn.Label, arrayToString(cdn.Ips), arrayToString(cdn.Hostnames))
+
+	expectedValidationError, _ := regexp.Compile(".*\"label\": required field is not set.*")
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckCDNsV1Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config:      testCDNCreateConfigMissingRequiredProperty,
+				ExpectError: expectedValidationError,
+			},
+		},
+	})
+}
+
 func TestAccCDN_Update(t *testing.T) {
-	log.Println(testCDNCreateConfig)
+	var cdnUpdated = newContentDeliveryNetwork(cdn.Label, cdn.Ips, cdn.Hostnames, 14, 14.14, false)
+	testCDNUpdatedConfig := populateTemplateConfiguration(cdnUpdated.Label, cdnUpdated.Ips, cdnUpdated.Hostnames, cdnUpdated.ExampleInt, cdnUpdated.ExampleNumber, cdnUpdated.ExampleBoolean)
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
@@ -225,6 +191,7 @@ func TestAccCDN_Update(t *testing.T) {
 }
 
 func TestAccCDN_UpdateImmutableProperty(t *testing.T) {
+	testCDNUpdatedImmutableConfig := populateTemplateConfiguration("label updated", cdn.Ips, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean)
 	expectedValidationError, _ := regexp.Compile(".*property label is immutable and therefore can not be updated. Update operation was aborted; no updates were performed.*")
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -262,6 +229,8 @@ func TestAccCDN_UpdateImmutableProperty(t *testing.T) {
 }
 
 func TestAccCDN_UpdateForceNewProperty(t *testing.T) {
+	var cdnUpdatedForceNew = newContentDeliveryNetwork(cdn.Label, []string{"192.168.1.5"}, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean)
+	testCDNUpdatedForceNewConfig := populateTemplateConfiguration(cdnUpdatedForceNew.Label, cdnUpdatedForceNew.Ips, cdnUpdatedForceNew.Hostnames, cdnUpdatedForceNew.ExampleInt, cdnUpdatedForceNew.ExampleNumber, cdnUpdatedForceNew.ExampleBoolean)
 	var originalId string
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -335,6 +304,34 @@ func TestAccCDN_UpdateForceNewProperty(t *testing.T) {
 			},
 		},
 	})
+}
+
+func newContentDeliveryNetwork(label string, ips, hostnames []string, exampleInt int32, exampleNumber float32, exampleBool bool) api.ContentDeliveryNetwork {
+	return api.ContentDeliveryNetwork{
+		Label:          label,
+		Ips:            ips,
+		Hostnames:      hostnames,
+		ExampleInt:     exampleInt,
+		ExampleNumber:  exampleNumber,
+		ExampleBoolean: exampleBool,
+	}
+}
+
+func populateTemplateConfiguration(label string, ips, hostnames []string, exampleInt int32, exampleNumber float32, exampleBool bool) string {
+	return fmt.Sprintf(`provider "%s" {
+  apikey_auth = "apiKeyValue"
+  x_request_id = "some value..."
+}
+
+resource "%s" "my_cdn" {
+  label = "%s"
+  ips = ["%s"]
+  hostnames = ["%s"]
+
+  example_int = %d
+  better_example_number_field_name = %s
+  example_boolean = %v
+}`, providerName, openAPIResourceName, label, arrayToString(ips), arrayToString(hostnames), exampleInt, floatToString(exampleNumber), exampleBool)
 }
 
 func floatToString(number float32) string {
