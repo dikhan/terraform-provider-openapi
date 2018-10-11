535b6b006e68ae0fce019eaf320187f35fc8bfe8
dikhan <di.khan.r@gmail.com>
2018-10-10 23:21:41 -0700

    add unit tests for sec def schemes

---
+++ openapi/openapi_spec_security_schemes_test.go
@@ -0,0 +1,76 @@
+package openapi
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSpecSecuritySchemeGetTerraformConfigurationName(t *testing.T) {
+	Convey("Given a SpecSecurityScheme with a terraform compliant name", t, func() {
+		expectedName := "some_compliant_name"
+		specSecurityScheme := SpecSecurityScheme{Name:expectedName}
+		Convey("When newAPIKeySecurityDefinition method is called", func() {
+			secSchemeTerraformName := specSecurityScheme.getTerraformConfigurationName()
+			Convey("Then the secSchemeTerraformName name should match", func() {
+				So(secSchemeTerraformName, ShouldEqual, expectedName)
+			})
+		})
+	})
+	Convey("Given a SpecSecurityScheme with a Non terraform compliant name", t, func() {
+		specSecurityScheme := SpecSecurityScheme{Name:"nonCompliantName"}
+		Convey("When newAPIKeySecurityDefinition method is called", func() {
+			secSchemeTerraformName := specSecurityScheme.getTerraformConfigurationName()
+			Convey("Then the secSchemeTerraformName name should be compliant", func() {
+				So(secSchemeTerraformName, ShouldEqual, "non_compliant_name")
+			})
+		})
+	})
+}
+
+func TestCreateSecuritySchemes(t *testing.T) {
+	Convey("Given a map of securitySchemes with multi auth AND support", t, func() {
+		securitySchemes := []map[string][]string{
+			map[string][]string{
+				"secDef1": []string{},
+				"secDef2": []string{},
+			},
+		}
+		Convey("When createSecuritySchemes method is called with the securitySchemes", func() {
+			specSecuritySchemes := createSecuritySchemes(securitySchemes)
+			Convey("Then the specSecuritySchemes should not be empty", func() {
+				So(specSecuritySchemes, ShouldNotBeEmpty)
+			})
+			Convey("Then the specSecuritySchemes name contain the expected items", func() {
+				So(specSecuritySchemes, ShouldContain, SpecSecurityScheme{Name: "secDef1"})
+				So(specSecuritySchemes, ShouldContain, SpecSecurityScheme{Name: "secDef2"})
+			})
+		})
+	})
+
+	Convey("Given a map of securitySchemes with multi auth OR support", t, func() {
+		securitySchemes := []map[string][]string{
+			map[string][]string{
+				"secDef1": []string{},
+				"secDef2": []string{},
+			},
+			map[string][]string{
+				"secDef3": []string{},
+			},
+		}
+		Convey("When createSecuritySchemes method is called with the securitySchemes", func() {
+			specSecuritySchemes := createSecuritySchemes(securitySchemes)
+			Convey("Then the specSecuritySchemes should not be empty", func() {
+				So(specSecuritySchemes, ShouldNotBeEmpty)
+			})
+			Convey("Then the specSecuritySchemes name contain the expected items which are the first one in the array (by design these take preference)", func() {
+				So(specSecuritySchemes, ShouldContain, SpecSecurityScheme{Name: "secDef1"})
+				So(specSecuritySchemes, ShouldContain, SpecSecurityScheme{Name: "secDef2"})
+			})
+			Convey("Then the specSecuritySchemes should not contain anything else", func() {
+				So(specSecuritySchemes, ShouldNotContain, SpecSecurityScheme{Name: "secDef3"})
+			})
+		})
+	})
+}
+
