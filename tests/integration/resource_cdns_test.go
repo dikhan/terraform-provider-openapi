f7a249bdbd495eec81507e28d3ba62d2c510ccae
dikhan <di.khan.r@gmail.com>
2018-11-29 16:09:22 -0800

    fix integration tests
    - add extra property to object example
    - add multiple items to the array of objects

--- tests/integration/resource_cdns_test.go
+++ tests/integration/resource_cdns_test.go
@@ -25,8 +25,8 @@ var testCreateConfigCDN string
 
 func init() {
 	// Setting this up here as it is used by many different tests
-	cdn = newContentDeliveryNetwork("someLabel", []string{"192.168.0.2"}, []string{"www.google.com"}, 10, 12.22, true, "some message news", "some_value_for_the_list_of_object")
-	testCreateConfigCDN = populateTemplateConfigurationCDN(cdn.Label, cdn.Ips, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean, cdn.ObjectProperty.Message, cdn.ArrayOfObjectsExample[0].Protocol)
+	cdn = newContentDeliveryNetwork("someLabel", []string{"192.168.0.2"}, []string{"www.google.com"}, 10, 12.22, true, "some updated message news", "some message news with details", "http", 80, "https", 443)
+	testCreateConfigCDN = populateTemplateConfigurationCDN(cdn.Label, cdn.Ips, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean, cdn.ObjectProperty.Message, cdn.ObjectProperty.DetailedMessage, cdn.ArrayOfObjectsExample[0].Protocol, cdn.ArrayOfObjectsExample[0].OriginPort, cdn.ArrayOfObjectsExample[1].Protocol, cdn.ArrayOfObjectsExample[1].OriginPort)
 }
 
 func TestAccCDN_Create(t *testing.T) {
@@ -56,15 +56,23 @@ func TestAccCDN_Create(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "example_boolean", fmt.Sprintf("%v", cdn.ExampleBoolean)),
 					resource.TestCheckResourceAttr(
-						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 1)),
+						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 2)),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_property.message", cdn.ObjectProperty.Message),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_property.detailed_message", cdn.ObjectProperty.DetailedMessage),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_nested_scheme_property.%", fmt.Sprintf("%d", 1)),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.#", fmt.Sprintf("%d", len(cdn.ArrayOfObjectsExample))),
 					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.0.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[0].OriginPort)),
+					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.0.protocol", cdn.ArrayOfObjectsExample[0].Protocol),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[1].OriginPort)),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.protocol", cdn.ArrayOfObjectsExample[1].Protocol),
 				),
 			},
 		},
@@ -138,9 +146,11 @@ func TestAccCDN_Import(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "example_boolean", fmt.Sprintf("%v", cdn.ExampleBoolean)),
 					resource.TestCheckResourceAttr(
-						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 1)),
+						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 2)),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_property.message", cdn.ObjectProperty.Message),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_property.detailed_message", cdn.ObjectProperty.DetailedMessage),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_nested_scheme_property.%", fmt.Sprintf("%d", 1)),
 					resource.TestCheckResourceAttr(
@@ -148,7 +158,13 @@ func TestAccCDN_Import(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.#", fmt.Sprintf("%d", len(cdn.ArrayOfObjectsExample))),
 					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.0.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[0].OriginPort)),
+					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.0.protocol", cdn.ArrayOfObjectsExample[0].Protocol),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[1].OriginPort)),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.protocol", cdn.ArrayOfObjectsExample[1].Protocol),
 				),
 			},
 			{
@@ -175,9 +191,11 @@ func TestAccCDN_Import(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "example_boolean", fmt.Sprintf("%v", cdn.ExampleBoolean)),
 					resource.TestCheckResourceAttr(
-						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 1)),
+						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 2)),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_property.message", cdn.ObjectProperty.Message),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_property.detailed_message", cdn.ObjectProperty.DetailedMessage),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_nested_scheme_property.%", fmt.Sprintf("%d", 1)),
 					resource.TestCheckResourceAttr(
@@ -185,7 +203,13 @@ func TestAccCDN_Import(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.#", fmt.Sprintf("%d", len(cdn.ArrayOfObjectsExample))),
 					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.0.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[0].OriginPort)),
+					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.0.protocol", cdn.ArrayOfObjectsExample[0].Protocol),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[1].OriginPort)),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.protocol", cdn.ArrayOfObjectsExample[1].Protocol),
 				),
 			},
 		},
@@ -264,8 +288,8 @@ resource "%s" "my_cdn" {
 }
 
 func TestAccCDN_Update(t *testing.T) {
-	var cdnUpdated = newContentDeliveryNetwork(cdn.Label, cdn.Ips, cdn.Hostnames, 14, 14.14, false, "some message news updated", "updated protocol")
-	testCDNUpdatedConfig := populateTemplateConfigurationCDN(cdnUpdated.Label, cdnUpdated.Ips, cdnUpdated.Hostnames, cdnUpdated.ExampleInt, cdnUpdated.ExampleNumber, cdnUpdated.ExampleBoolean, cdnUpdated.ObjectProperty.Message, cdnUpdated.ArrayOfObjectsExample[0].Protocol)
+	var cdnUpdated = newContentDeliveryNetwork(cdn.Label, cdn.Ips, cdn.Hostnames, 14, 14.14, false, "some updated message news", "some message news with details", "http", 80, "https", 443)
+	testCDNUpdatedConfig := populateTemplateConfigurationCDN(cdnUpdated.Label, cdnUpdated.Ips, cdnUpdated.Hostnames, cdnUpdated.ExampleInt, cdnUpdated.ExampleNumber, cdnUpdated.ExampleBoolean, cdnUpdated.ObjectProperty.Message, cdnUpdated.ObjectProperty.DetailedMessage, cdnUpdated.ArrayOfObjectsExample[0].Protocol, cdnUpdated.ArrayOfObjectsExample[0].OriginPort, cdnUpdated.ArrayOfObjectsExample[1].Protocol, cdnUpdated.ArrayOfObjectsExample[1].OriginPort)
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
@@ -292,9 +316,11 @@ func TestAccCDN_Update(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "example_boolean", fmt.Sprintf("%v", cdn.ExampleBoolean)),
 					resource.TestCheckResourceAttr(
-						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 1)),
+						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 2)),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_property.message", cdn.ObjectProperty.Message),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_property.detailed_message", cdn.ObjectProperty.DetailedMessage),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_nested_scheme_property.%", fmt.Sprintf("%d", 1)),
 					resource.TestCheckResourceAttr(
@@ -302,7 +328,13 @@ func TestAccCDN_Update(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.#", fmt.Sprintf("%d", len(cdn.ArrayOfObjectsExample))),
 					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.0.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[0].OriginPort)),
+					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.0.protocol", cdn.ArrayOfObjectsExample[0].Protocol),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[1].OriginPort)),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.protocol", cdn.ArrayOfObjectsExample[1].Protocol),
 				),
 			},
 			{
@@ -326,9 +358,11 @@ func TestAccCDN_Update(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "example_boolean", fmt.Sprintf("%v", cdnUpdated.ExampleBoolean)),
 					resource.TestCheckResourceAttr(
-						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 1)),
+						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 2)),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_property.message", cdnUpdated.ObjectProperty.Message),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_property.detailed_message", cdnUpdated.ObjectProperty.DetailedMessage),
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "object_nested_scheme_property.%", fmt.Sprintf("%d", 1)),
 					resource.TestCheckResourceAttr(
@@ -336,7 +370,13 @@ func TestAccCDN_Update(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.#", fmt.Sprintf("%d", len(cdnUpdated.ArrayOfObjectsExample))),
 					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.0.origin_port", fmt.Sprint(cdnUpdated.ArrayOfObjectsExample[0].OriginPort)),
+					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.0.protocol", cdnUpdated.ArrayOfObjectsExample[0].Protocol),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.origin_port", fmt.Sprint(cdnUpdated.ArrayOfObjectsExample[1].OriginPort)),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.protocol", cdnUpdated.ArrayOfObjectsExample[1].Protocol),
 				),
 			},
 		},
@@ -344,8 +384,8 @@ func TestAccCDN_Update(t *testing.T) {
 }
 
 func TestAccCDN_CreateWithZeroValues(t *testing.T) {
-	var cdn = newContentDeliveryNetwork("", []string{}, []string{}, 0, 0, false, "some message news", "")
-	testCDNZeroValuesConfig := populateTemplateConfigurationCDN(cdn.Label, cdn.Ips, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean, cdn.ObjectProperty.Message, cdn.ArrayOfObjectsExample[0].Protocol)
+	var cdn = newContentDeliveryNetwork("label", []string{}, []string{}, 0, 0, false, "some message news", "some message news with details", "http", 80, "https", 443)
+	testCDNZeroValuesConfig := populateTemplateConfigurationCDN(cdn.Label, cdn.Ips, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean, cdn.ObjectProperty.Message, cdn.ObjectProperty.DetailedMessage, cdn.ArrayOfObjectsExample[0].Protocol, cdn.ArrayOfObjectsExample[0].OriginPort, cdn.ArrayOfObjectsExample[1].Protocol, cdn.ArrayOfObjectsExample[1].OriginPort)
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
@@ -380,7 +420,7 @@ func TestAccCDN_CreateWithZeroValues(t *testing.T) {
 }
 
 func TestAccCDN_UpdateImmutableProperty(t *testing.T) {
-	testCDNUpdatedImmutableConfig := populateTemplateConfigurationCDN("label updated", cdn.Ips, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean, cdn.ObjectProperty.Message, "")
+	testCDNUpdatedImmutableConfig := populateTemplateConfigurationCDN("label updated", cdn.Ips, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean, cdn.ObjectProperty.Message, cdn.ObjectProperty.DetailedMessage, cdn.ArrayOfObjectsExample[0].Protocol, cdn.ArrayOfObjectsExample[0].OriginPort, cdn.ArrayOfObjectsExample[1].Protocol, cdn.ArrayOfObjectsExample[1].OriginPort)
 	expectedValidationError, _ := regexp.Compile(".*property label is immutable and therefore can not be updated. Update operation was aborted; no updates were performed.*")
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -418,8 +458,8 @@ func TestAccCDN_UpdateImmutableProperty(t *testing.T) {
 }
 
 func TestAccCDN_UpdateForceNewProperty(t *testing.T) {
-	var cdnUpdatedForceNew = newContentDeliveryNetwork(cdn.Label, []string{"192.168.1.5"}, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean, "some message news", cdn.ArrayOfObjectsExample[0].Protocol)
-	testCDNUpdatedForceNewConfig := populateTemplateConfigurationCDN(cdnUpdatedForceNew.Label, cdnUpdatedForceNew.Ips, cdnUpdatedForceNew.Hostnames, cdnUpdatedForceNew.ExampleInt, cdnUpdatedForceNew.ExampleNumber, cdnUpdatedForceNew.ExampleBoolean, cdnUpdatedForceNew.ObjectProperty.Message, cdnUpdatedForceNew.ArrayOfObjectsExample[0].Protocol)
+	var cdnUpdatedForceNew = newContentDeliveryNetwork(cdn.Label, []string{"192.168.1.5"}, cdn.Hostnames, cdn.ExampleInt, cdn.ExampleNumber, cdn.ExampleBoolean, "some message news", "some message news with details", "http", 8080, "https", 8443)
+	testCDNUpdatedForceNewConfig := populateTemplateConfigurationCDN(cdnUpdatedForceNew.Label, cdnUpdatedForceNew.Ips, cdnUpdatedForceNew.Hostnames, cdnUpdatedForceNew.ExampleInt, cdnUpdatedForceNew.ExampleNumber, cdnUpdatedForceNew.ExampleBoolean, cdnUpdatedForceNew.ObjectProperty.Message, cdnUpdatedForceNew.ObjectProperty.DetailedMessage, cdnUpdatedForceNew.ArrayOfObjectsExample[0].Protocol, cdnUpdatedForceNew.ArrayOfObjectsExample[0].OriginPort, cdnUpdatedForceNew.ArrayOfObjectsExample[1].Protocol, cdnUpdatedForceNew.ArrayOfObjectsExample[1].OriginPort)
 	var originalID string
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -455,9 +495,25 @@ func TestAccCDN_UpdateForceNewProperty(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "example_boolean", fmt.Sprintf("%v", cdn.ExampleBoolean)),
 					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_property.%", fmt.Sprintf("%d", 2)),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_property.message", cdn.ObjectProperty.Message),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_property.detailed_message", cdn.ObjectProperty.DetailedMessage),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_nested_scheme_property.%", fmt.Sprintf("%d", 1)),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "object_nested_scheme_property.name", "autogenerated name"),
+					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.#", fmt.Sprintf("%d", len(cdn.ArrayOfObjectsExample))),
 					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.0.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[0].OriginPort)),
+					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.0.protocol", cdn.ArrayOfObjectsExample[0].Protocol),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.origin_port", fmt.Sprint(cdn.ArrayOfObjectsExample[1].OriginPort)),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.protocol", cdn.ArrayOfObjectsExample[1].Protocol),
 				),
 			},
 			{
@@ -496,14 +552,20 @@ func TestAccCDN_UpdateForceNewProperty(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.#", fmt.Sprintf("%d", len(cdnUpdatedForceNew.ArrayOfObjectsExample))),
 					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.0.origin_port", fmt.Sprint(cdnUpdatedForceNew.ArrayOfObjectsExample[0].OriginPort)),
+					resource.TestCheckResourceAttr(
 						openAPIResourceStateCDN, "array_of_objects_example.0.protocol", cdnUpdatedForceNew.ArrayOfObjectsExample[0].Protocol),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.origin_port", fmt.Sprint(cdnUpdatedForceNew.ArrayOfObjectsExample[1].OriginPort)),
+					resource.TestCheckResourceAttr(
+						openAPIResourceStateCDN, "array_of_objects_example.1.protocol", cdnUpdatedForceNew.ArrayOfObjectsExample[1].Protocol),
 				),
 			},
 		},
 	})
 }
 
-func newContentDeliveryNetwork(label string, ips, hostnames []string, exampleInt int32, exampleNumber float32, exampleBool bool, objectPropertyMessage, listObjectProtocolValue string) api.ContentDeliveryNetworkV1 {
+func newContentDeliveryNetwork(label string, ips, hostnames []string, exampleInt int32, exampleNumber float32, exampleBool bool, objectPropertyMessage, objectDetailedMessage, listObjectProtocol string, listObjectOriginPort int32, listObject2Protocol string, listObject2OriginPort int32) api.ContentDeliveryNetworkV1 {
 	return api.ContentDeliveryNetworkV1{
 		Label:                 label,
 		Ips:                   ips,
@@ -511,12 +573,12 @@ func newContentDeliveryNetwork(label string, ips, hostnames []string, exampleInt
 		ExampleInt:            exampleInt,
 		ExampleNumber:         exampleNumber,
 		ExampleBoolean:        exampleBool,
-		ObjectProperty:        &api.ObjectProperty{Message: objectPropertyMessage},
-		ArrayOfObjectsExample: []api.ContentDeliveryNetworkV1ArrayOfObjectsExample{{listObjectProtocolValue}},
+		ObjectProperty:        &api.ObjectProperty{Message: objectPropertyMessage, DetailedMessage: objectDetailedMessage},
+		ArrayOfObjectsExample: []api.ContentDeliveryNetworkV1ArrayOfObjectsExample{{Protocol: listObjectProtocol, OriginPort: listObjectOriginPort}, {Protocol: listObject2Protocol, OriginPort: listObject2OriginPort}},
 	}
 }
 
-func populateTemplateConfigurationCDN(label string, ips, hostnames []string, exampleInt int32, exampleNumber float32, exampleBool bool, objectPropertyMessage, listObjectMessage string) string {
+func populateTemplateConfigurationCDN(label string, ips, hostnames []string, exampleInt int32, exampleNumber float32, exampleBool bool, objectPropertyMessage, objectDetailedMessage, listObjectProtocol string, listObjectOriginPort int32, listObject2Protocol string, listObject2OriginPort int32) string {
 	return fmt.Sprintf(`provider "%s" {
   apikey_auth = "apiKeyValue"
   x_request_id = "some value..."
@@ -533,14 +595,20 @@ resource "%s" "%s" {
 
   object_property = {
     message = "%s"
+    detailed_message = "%s"
   }
 
   array_of_objects_example = [
     {
       protocol = "%s"
+      origin_port = %d
+    },
+    {
+      protocol = "%s"
+      origin_port = %d
     }
   ]
-}`, providerName, openAPIResourceNameCDN, openAPIResourceInstanceNameCDN, label, arrayToString(ips), arrayToString(hostnames), exampleInt, floatToString(exampleNumber), exampleBool, objectPropertyMessage, listObjectMessage)
+}`, providerName, openAPIResourceNameCDN, openAPIResourceInstanceNameCDN, label, arrayToString(ips), arrayToString(hostnames), exampleInt, floatToString(exampleNumber), exampleBool, objectPropertyMessage, objectDetailedMessage, listObjectProtocol, listObjectOriginPort, listObject2Protocol, listObject2OriginPort)
 }
 
 // Acceptance test resource-destruction for openapi_cdn_v1:
