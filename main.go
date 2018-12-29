bc203c4de82df8829373244bb902d78276333b27
dikhan <di.khan.r@gmail.com>
2018-12-29 09:51:20 -0800

    use the same instance of provider within ProviderFunc
    - having the instance being constructed within the ProviderFunc was causing
    terraform to create a new instance anytime the method was called resulting
    into all kind of behaviour and performance issues

--- main.go
+++ main.go
@@ -30,14 +30,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("[ERROR] There was an error when getting the provider's name fomr the binary '%s': %s", ex, err)
 	}
+
+	p := openapi.ProviderOpenAPI{ProviderName: providerName}
+	provider, err := p.CreateSchemaProvider()
+	if err != nil {
+		log.Fatalf("[ERROR] There was an error initialising the terraform provider: %s", err)
+	}
+
 	plugin.Serve(
 		&plugin.ServeOpts{
 			ProviderFunc: func() terraform.ResourceProvider {
-				p := openapi.ProviderOpenAPI{ProviderName: providerName}
-				provider, err := p.CreateSchemaProvider()
-				if err != nil {
-					log.Fatalf("[ERROR] There was an error initialising the terraform provider: %s", err)
-				}
 				return provider
 			},
 		})
