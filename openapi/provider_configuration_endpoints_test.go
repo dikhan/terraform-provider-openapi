1b76e59c3f77cae87bf4daa7a6c540a1393957fa
dikhan <di.khan.r@gmail.com>
2019-03-28 16:33:09 -0700

    add unit tests for endpoints
    - missing configureEndpoints due to complexity creating a test terraform
    SET object to unit test with. Instead, this logic is covered in integration
    tests

---
+++ openapi/provider_configuration_endpoints_test.go
@@ -0,0 +1,210 @@
+package openapi
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/hashicorp/terraform/helper/hashcode"
+	"github.com/hashicorp/terraform/helper/schema"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGetResourceNames(t *testing.T) {
+	Convey("Given a provider configuration endpoints configured with a spec analyser that has one resource", t, func() {
+		expectedResourceName := "resource_name_v1"
+		resource := newSpecStubResource(expectedResourceName, "", false, nil)
+		p := providerConfigurationEndPoints{
+			specAnalyser: &specAnalyserStub{
+				resources: []SpecResource{resource},
+				headers:   SpecHeaderParameters{},
+				security: &specSecurityStub{
+					securityDefinitions:   &SpecSecurityDefinitions{},
+					globalSecuritySchemes: createSecuritySchemes([]map[string][]string{}),
+				},
+			},
+		}
+		Convey("When getResourceNames is called with a map containing some resources ", func() {
+			resources, err := p.getResourceNames()
+			Convey("Then the error returned should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("Then the list of resource should not be empty", func() {
+				So(resources, ShouldNotBeEmpty)
+			})
+			Convey("And should match the expected number of resources", func() {
+				So(len(resources), ShouldEqual, 1)
+			})
+			Convey("And the list should contain the expected resources", func() {
+				So(resources, ShouldContain, expectedResourceName)
+			})
+		})
+	})
+
+	Convey("Given a provider configuration endpoints configured with a spec analyser that has NO resources", t, func() {
+		p := providerConfigurationEndPoints{
+			specAnalyser: &specAnalyserStub{
+				resources: []SpecResource{},
+				headers:   SpecHeaderParameters{},
+				security: &specSecurityStub{
+					securityDefinitions:   &SpecSecurityDefinitions{},
+					globalSecuritySchemes: createSecuritySchemes([]map[string][]string{}),
+				},
+			},
+		}
+		Convey("When getResourceNames is called with an empty map", func() {
+			resources, err := p.getResourceNames()
+			Convey("Then the error returned should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("Then the list of resource should be empty", func() {
+				So(resources, ShouldBeEmpty)
+			})
+		})
+	})
+
+}
+
+func TestEndpointsSchema(t *testing.T) {
+	Convey("Given a provider factory with a spec analyser that has one resource", t, func() {
+		resourceName := "resource_name_v1"
+		resource := newSpecStubResource(resourceName, "", false, nil)
+		p := providerConfigurationEndPoints{
+			specAnalyser: &specAnalyserStub{
+				resources: []SpecResource{resource},
+				headers:   SpecHeaderParameters{},
+				security: &specSecurityStub{
+					securityDefinitions:   &SpecSecurityDefinitions{},
+					globalSecuritySchemes: createSecuritySchemes([]map[string][]string{}),
+				},
+			},
+		}
+		Convey("When endpointsSchema is called", func() {
+			resourceName := "resource_name_v1"
+			s, err := p.endpointsSchema()
+			Convey("Then the error returned should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("Then the schema returned should NOT be nil", func() {
+				So(s, ShouldNotBeNil)
+			})
+			Convey("And the schema type should be a set", func() {
+				So(s.Type, ShouldEqual, schema.TypeSet)
+			})
+			Convey("And the schema should be optional", func() {
+				So(s.Optional, ShouldBeTrue)
+			})
+			Convey("And the schema element should be of type schema resource", func() {
+				So(s.Elem, ShouldHaveSameTypeAs, &schema.Resource{})
+			})
+			Convey("And the schema element resource schema should contain the expected resource", func() {
+				So(s.Elem.(*schema.Resource).Schema, ShouldContainKey, resourceName)
+			})
+			Convey("And the schema Set should not be nil (this defines the unique ID )", func() {
+				So(s.Set, ShouldNotBeNil)
+			})
+		})
+	})
+	Convey("Given a provider factory with a spec analyser that has no resources", t, func() {
+		p := providerConfigurationEndPoints{
+			specAnalyser: &specAnalyserStub{
+				resources: []SpecResource{},
+				headers:   SpecHeaderParameters{},
+				security: &specSecurityStub{
+					securityDefinitions:   &SpecSecurityDefinitions{},
+					globalSecuritySchemes: createSecuritySchemes([]map[string][]string{}),
+				},
+			},
+		}
+		Convey("When endpointsSchema is called", func() {
+			s, err := p.endpointsSchema()
+			Convey("Then the error returned should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("Then the schema returned should be nil", func() {
+				So(s, ShouldBeNil)
+			})
+		})
+	})
+}
+
+func TestEndpointsToHash(t *testing.T) {
+	Convey("Given a provider factory", t, func() {
+		p := providerConfigurationEndPoints{
+			specAnalyser: &specAnalyserStub{
+				headers: SpecHeaderParameters{},
+				security: &specSecurityStub{
+					securityDefinitions:   &SpecSecurityDefinitions{},
+					globalSecuritySchemes: createSecuritySchemes([]map[string][]string{}),
+				},
+			},
+		}
+		Convey("When endpointsSchema is called with a list of resources", func() {
+			resourceName := "resource_name_v1"
+			schemaSetFunction := p.endpointsToHash([]string{resourceName})
+			Convey("Then the schema set function returned should NOT be nil", func() {
+				So(schemaSetFunction, ShouldNotBeNil)
+			})
+			Convey("And the return int from calling the schemaSetFunction() shuold be the expected one", func() {
+				m := map[string]interface{}{}
+				m[resourceName] = "something to get the string represention from"
+				var buf bytes.Buffer
+				buf.WriteString(fmt.Sprintf("%s-", m[resourceName].(string)))
+				So(schemaSetFunction(m), ShouldEqual, hashcode.String(buf.String()))
+			})
+		})
+	})
+}
+
+
+//func TestGetProviderConfigEndPointsFromData(t *testing.T) {
+//	Convey("Given a provider factory", t, func() {
+//		expectedResource := "resource_name"
+//		p := providerConfigurationEndPoints{
+//			specAnalyser: &specAnalyserStub{
+//				resources: []SpecResource{
+//					newSpecStubResource(expectedResource, "", false, nil),
+//				},
+//				headers: SpecHeaderParameters{},
+//				security: &specSecurityStub{
+//					securityDefinitions:   &SpecSecurityDefinitions{},
+//					globalSecuritySchemes: createSecuritySchemes([]map[string][]string{}),
+//				},
+//			},
+//		}
+//
+//		resourceSchema := map[string]*schema.Schema{}
+//		resourceDataMap := map[string]interface{}{}
+//
+//		s, err := p.endpointsSchema()
+//		So(err, ShouldBeNil)
+//
+//		resourceSchema[providerPropertyEndPoints] = s
+//
+//		expectedValue := "resource_value"
+//		e := map[string]interface{}{
+//			expectedResource: expectedValue,
+//		}
+//
+//		hash := p.endpointsToHash([]string{expectedResource})(e)
+//
+//
+//		resourceDataMap[providerPropertyEndPoints] = map[string]interface{}{
+//			fmt.Sprintf("%d", hash): map[string]interface{}{
+//				expectedResource: expectedValue,
+//			},
+//		}
+//		resourceLocalData := schema.TestResourceDataRaw(t, resourceSchema, resourceDataMap)
+//
+//		Convey("When configureEndpoints is called with a list of resources", func() {
+//			resourceName := "resource_name_v1"
+//			endpoints, err := p.configureEndpoints(resourceLocalData)
+//			Convey("Then the error returned should not be nil", func() {
+//				So(err, ShouldBeNil)
+//			})
+//			Convey("And the endpoints returned should contain the resource with the value expected", func() {
+//				So(endpoints, ShouldContainKey, resourceName)
+//			})
+//		})
+//	})
+//}
