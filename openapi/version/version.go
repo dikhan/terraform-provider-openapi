f5a064d5f83c3fcc62513d0e3bc4dec2c6e7ac8c
dikhan <di.khan.r@gmail.com>
2019-05-07 17:49:19 -0700

    create BuildUserAgent using version and commit info

--- openapi/version/version.go
+++ openapi/version/version.go
@@ -1,7 +1,15 @@
 package version
 
+import (
+	"fmt"
+)
+
 var (
 	Version = "dev"
 	Commit  = "none"
 	Date    = "unknown"
 )
+
+func BuildUserAgent(runtime, arch string) string {
+	return fmt.Sprintf("OpenAPI Terraform Provider/%s-%s (%s/%s)", Version, Commit, runtime, arch)
+}
