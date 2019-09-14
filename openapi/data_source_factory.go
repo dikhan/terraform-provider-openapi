7bf7c12485b922b22b62139448f98e06314378ed
dikhan <di.khan.r@gmail.com>
2019-09-13 17:46:07 -0700

    flesh out todo comment

--- openapi/data_source_factory.go
+++ openapi/data_source_factory.go
@@ -40,7 +40,9 @@ func (d dataSourceFactory) createTerraformDataSourceSchema() map[string]*schema.
 	return map[string]*schema.Schema{
 		dataSourceFilterPropertyName: d.dataSourceFiltersSchema(),
 		// TODO: need to populate also here the properties for the data source so then we can update the state object (data *schema.ResourceData)
-		//  at the end of the read operation
+		//  at the end of the read operation. This can be done via getting the schema from d.openAPIResource.getResourceSchema() and
+		//  calling createResourceSchema(). Some adjustments will need to be made making all the properties computed
+		//  no matter what the schema is configured with since the model might be the same as the actual resource.
 	}
 }
 
