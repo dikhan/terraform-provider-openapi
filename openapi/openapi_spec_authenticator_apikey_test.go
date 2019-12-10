1f139b30c11f4f723dbee69918a8b4d6e503a2ef
dikhan <di.khan.r@gmail.com>
2019-12-10 10:06:38 -0800

    refactor test to use table driven

--- openapi/openapi_spec_authenticator_apikey_test.go
+++ openapi/openapi_spec_authenticator_apikey_test.go
@@ -1,51 +1,49 @@
 package openapi
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
-
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestCreateAPIKeyAuthenticator(t *testing.T) {
-	Convey("Given a secDef of header type and a auth value ", t, func() {
-		secDef := newAPIKeyHeaderSecurityDefinition("header_auth", authorizationHeader)
-		value := "value"
-		Convey("When createAPIKeyAuthenticator method is constructed", func() {
-			apiKeyAuthenticator := createAPIKeyAuthenticator(secDef, value)
-			Convey("And the the specAPIKeyAuthenticator returned Should Have Same Type As apiKeyHeaderAuthenticator", func() {
-				So(apiKeyAuthenticator, ShouldHaveSameTypeAs, apiKeyHeaderAuthenticator{})
-			})
-			Convey("And the the specAPIKeyAuthenticator returned should be of type authTypeAPIKeyHeader", func() {
-				So(apiKeyAuthenticator.getType(), ShouldEqual, authTypeAPIKeyHeader)
-			})
-		})
-	})
+	testCases := []struct {
+		name                    string
+		secDef                  SpecSecurityDefinition
+		value                   string
+		expectedAuthType        specAPIKeyAuthenticator
+		expectedType            authType
+		expectedValidationError error
+	}{
+		{
+			name:                    "createAPIKeyAuthenticator is called with a valid specAPIKeyHeaderSecurityDefinition and a value",
+			secDef:                  newAPIKeyHeaderSecurityDefinition("header_auth", authorizationHeader),
+			value:                   "value",
+			expectedAuthType:        apiKeyHeaderAuthenticator{},
+			expectedType:            authTypeAPIKeyHeader,
+			expectedValidationError: nil,
+		},
+		{
+			name:                    "createAPIKeyAuthenticator is called with a valid specAPIKeyQuerySecurityDefinition and a value",
+			secDef:                  newAPIKeyQuerySecurityDefinition("query_auth", authorizationHeader),
+			value:                   "value",
+			expectedAuthType:        apiKeyQueryAuthenticator{},
+			expectedType:            authTypeAPIQuery,
+			expectedValidationError: nil,
+		},
+		{
+			name:                    "createAPIKeyAuthenticator is called with a valid specAPIKeyHeaderRefreshTokenSecurityDefinition and a value",
+			secDef:                  newAPIKeyHeaderRefreshTokenSecurityDefinition("header_auth", authorizationHeader),
+			value:                   "value",
+			expectedAuthType:        apiRefreshTokenAuthenticator{},
+			expectedType:            authTypeAPIKeyHeader,
+			expectedValidationError: nil,
+		},
+	}
 
-	Convey("Given a secDef of query type and a auth value ", t, func() {
-		secDef := newAPIKeyQuerySecurityDefinition("query_auth", authorizationHeader)
-		value := "value"
-		Convey("When createAPIKeyAuthenticator method is constructed", func() {
-			apiKeyAuthenticator := createAPIKeyAuthenticator(secDef, value)
-			Convey("And the the specAPIKeyAuthenticator returned Should Have Same Type As apiKeyQueryAuthenticator", func() {
-				So(apiKeyAuthenticator, ShouldHaveSameTypeAs, apiKeyQueryAuthenticator{})
-			})
-			Convey("And the the specAPIKeyAuthenticator returned should be of type authTypeAPIQuery", func() {
-				So(apiKeyAuthenticator.getType(), ShouldEqual, authTypeAPIQuery)
-			})
-		})
-	})
-
-	Convey("Given a RefreshToken secDef of header type and a auth value ", t, func() {
-		secDef := newAPIKeyHeaderRefreshTokenSecurityDefinition("header_auth", authorizationHeader)
-		value := "value"
-		Convey("When createAPIKeyAuthenticator method is constructed", func() {
-			apiKeyAuthenticator := createAPIKeyAuthenticator(secDef, value)
-			Convey("And the the specAPIKeyAuthenticator returned Should Have Same Type As apiKeyHeaderAuthenticator", func() {
-				So(apiKeyAuthenticator, ShouldHaveSameTypeAs, apiRefreshTokenAuthenticator{})
-			})
-			Convey("And the the specAPIKeyAuthenticator returned should be of type authTypeAPIKeyHeader", func() {
-				So(apiKeyAuthenticator.getType(), ShouldEqual, authTypeAPIKeyHeader)
-			})
-		})
-	})
+	for _, tc := range testCases {
+		apiKeyAuthenticator := createAPIKeyAuthenticator(tc.secDef, tc.value)
+		assert.IsType(t, tc.expectedAuthType, apiKeyAuthenticator, tc.name)
+		assert.Equal(t, tc.expectedType, apiKeyAuthenticator.getType(), tc.name)
+		assert.Equal(t, tc.expectedValidationError, apiKeyAuthenticator.validate(), tc.name)
+	}
 }
