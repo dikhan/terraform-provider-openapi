c5825010d2af9857ac48cdefe60935a7d3fdb24d
lchan <lillian@lillchan.com>
2019-12-27 14:34:22 -0800

    add TestTelemetryProviderGraphite_Validate

--- openapi/plugin_config_telemetry_test.go
+++ openapi/plugin_config_telemetry_test.go
@@ -1,1 +1,43 @@
 package openapi
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTelemetryProviderGraphite_Validate(t *testing.T) {
+	testCases := []struct {
+		testName    string
+		host        string
+		port        int
+		expectedErr error
+	}{
+		{
+			testName: "happy path - host and port populated",
+			host:     "telemetry.myhost.com",
+			port:     8125,
+		},
+		{
+			testName:    "crappy path - host is empty",
+			host:        "",
+			port:        8125,
+			expectedErr: errors.New("graphite telemetry configuration is missing a value for the 'host property'"),
+		},
+		{
+			testName:    "crappy path - port is 0",
+			host:        "telemetry.myhost.com",
+			port:        0,
+			expectedErr: errors.New("graphite telemetry configuration is missing a valid value (>0) for the 'port' property'"),
+		},
+	}
+
+	for _, tc := range testCases {
+		tpg := TelemetryProviderGraphite{
+			Host: tc.host,
+			Port: tc.port,
+		}
+		err := tpg.Validate()
+		assert.Equal(t, tc.expectedErr, err, tc.testName)
+	}
+}
