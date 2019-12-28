3c8c8038dfeaf4711f706954200baabfdc3352a7
lchan <lillian@lillchan.com>
2019-12-27 17:20:20 -0800

    add happy path tests for IncOpenAPIPluginVersionTotalRunsCounter and IncServiceProviderTotalRunsCounter

--- openapi/plugin_config_telemetry_test.go
+++ openapi/plugin_config_telemetry_test.go
@@ -1,8 +1,12 @@
 package openapi
 
 import (
+	"bytes"
 	"errors"
 	"github.com/stretchr/testify/assert"
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -14,9 +18,10 @@ func TestTelemetryProviderGraphite_Validate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			testName: "happy path - host and port populated",
-			host:     "telemetry.myhost.com",
-			port:     8125,
+			testName:    "happy path - host and port populated",
+			host:        "telemetry.myhost.com",
+			port:        8125,
+			expectedErr: nil,
 		},
 		{
 			testName:    "crappy path - host is empty",
@@ -40,4 +45,44 @@ func TestTelemetryProviderGraphite_Validate(t *testing.T) {
 		err := tpg.Validate()
 		assert.Equal(t, tc.expectedErr, err, tc.testName)
 	}
+}
+
+func TestTelemetryProviderGraphite_IncOpenAPIPluginVersionTotalRunsCounter(t *testing.T) {
+	openAPIPluginVersion := "0.25.0"
+	expectedLogMetricToSubmit := "[INFO] graphite metric to be submitted: terraform.openapi_plugin_version.0_25_0.total_runs"
+	expectedLogMetricSuccess := "[INFO] graphite metric successfully submitted: terraform.openapi_plugin_version.0_25_0.total_runs"
+
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	tpg := createTestGraphiteProvider()
+	err := tpg.IncOpenAPIPluginVersionTotalRunsCounter(openAPIPluginVersion)
+
+	assert.Nil(t, err)
+	assert.Contains(t, buf.String(), expectedLogMetricToSubmit)
+	assert.Contains(t, buf.String(), expectedLogMetricSuccess)
+}
+
+func TestTelemetryProviderGraphite_IncServiceProviderTotalRunsCounter(t *testing.T) {
+	providerName := "myProviderName"
+	expectedLogMetricToSubmit := "[INFO] graphite metric to be submitted: terraform.providers.myProviderName.total_runs"
+	expectedLogMetricSuccess := "[INFO] graphite metric successfully submitted: terraform.providers.myProviderName.total_runs"
+
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	tpg := createTestGraphiteProvider()
+	err := tpg.IncServiceProviderTotalRunsCounter(providerName)
+
+	assert.Nil(t, err)
+	assert.Contains(t, buf.String(), expectedLogMetricToSubmit)
+	assert.Contains(t, buf.String(), expectedLogMetricSuccess)
+}
+
+func createTestGraphiteProvider() TelemetryProviderGraphite {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	tpg := TelemetryProviderGraphite{
+		Host:   s.URL[7:16],
+		Port:   8125,
+		Prefix: "myPrefixName",
+	}
+	return tpg
 }
