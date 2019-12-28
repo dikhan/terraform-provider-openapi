36d7c49c9251eeb56230d737b490b75b4ffda643
dikhan <di.khan.r@gmail.com>
2019-12-27 16:44:09 -0800

    Add GetTelemetryHandler in charge of building TelemetryHandler with validated telemetry providers

--- openapi/plugin_config_schema.go
+++ openapi/plugin_config_schema.go
@@ -2,7 +2,9 @@ package openapi
 
 import (
 	"fmt"
+	"github.com/dikhan/terraform-provider-openapi/openapi/version"
 	"gopkg.in/yaml.v2"
+	"log"
 )
 
 // ServiceConfigurations contains the map with all service configurations
@@ -20,6 +22,8 @@ type PluginConfigSchema interface {
 	GetVersion() (string, error)
 	// Marshal serializes the value provided into a YAML document
 	Marshal() ([]byte, error)
+
+	// TODO: GetTelemetryHandler() TelemetryHandler
 }
 
 // PluginConfigSchemaV1 defines PluginConfigSchema version 1
@@ -86,4 +90,25 @@ func (p *PluginConfigSchemaV1) GetAllServiceConfigurations() (ServiceConfigurati
 func (p *PluginConfigSchemaV1) Marshal() ([]byte, error) {
 	out, err := yaml.Marshal(p)
 	return out, err
+}
+
+// GetTelemetryHandler returns a handler containing validated telemetry providers
+func (p *PluginConfigSchemaV1) GetTelemetryHandler(providerName string) TelemetryHandler {
+	var telemetryProviders []TelemetryProvider
+	if p.TelemetryConfig != nil {
+		if p.TelemetryConfig.Graphite != nil {
+			err := p.TelemetryConfig.Graphite.Validate()
+			if err != nil {
+				log.Printf("[WARN] ignoring graphite telemetry due to the following validation error: %s", err)
+			} else {
+				telemetryProviders = append(telemetryProviders, p.TelemetryConfig.Graphite)
+				log.Printf("[INFO] graphite telemetry provider enabled")
+			}
+		}
+	}
+	return telemetryHandlerTimeoutSupport{
+		providerName:       providerName,
+		openAPIVersion:     version.Version,
+		telemetryProviders: telemetryProviders,
+	}
 }
