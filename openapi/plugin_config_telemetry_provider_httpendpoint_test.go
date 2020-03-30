38ac3217637d859d58b5db3c7cc813e2d622e417
lchan <lillian@lillchan.com>
2020-03-30 14:40:10 -0700

    Add TestCreateNewRequest with happy path

--- openapi/plugin_config_telemetry_provider_httpendpoint_test.go
+++ openapi/plugin_config_telemetry_provider_httpendpoint_test.go
@@ -1,8 +1,11 @@
 package openapi
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
@@ -35,5 +38,42 @@ func TestTelemetryProviderHttpEndpoint_Validate(t *testing.T) {
 		}
 		err := tpg.Validate()
 		assert.Equal(t, tc.expectedErr, err, tc.testName)
+	}
+}
+
+func TestCreateNewRequest(t *testing.T) {
+	testCases := []struct {
+		testName              string
+		expectedCounterMetric telemetryMetric
+		expectedReqHeader     http.Header
+		expectedErr           error
+	}{
+		{
+			testName: "happy path",
+			expectedCounterMetric: telemetryMetric{
+				MetricType: metricTypeCounter,
+				MetricName: "prefix.terraform.openapi_plugin_version.version.total_runs",
+			},
+			expectedReqHeader: http.Header{
+				"Content-Type": []string{"application/json"},
+				"User-Agent":   []string{"OpenAPI Terraform Provider/dev-none (darwin/amd64)"},
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		telemetryMetric := telemetryMetric{}
+		tph := TelemetryProviderHttpEndpoint{}
+
+		request, err := tph.createNewRequest(tc.expectedCounterMetric)
+		assert.Nil(t, err)
+		reqBody, err := ioutil.ReadAll(request.Body)
+		assert.Nil(t, err)
+		err = json.Unmarshal(reqBody, &telemetryMetric)
+		assert.Nil(t, err)
+
+		assert.Equal(t, tc.expectedReqHeader, request.Header)
+		assert.Equal(t, tc.expectedCounterMetric, telemetryMetric)
 	}
 }
