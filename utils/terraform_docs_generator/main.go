8f009244fcb7582e97836bed6b86e1967e80d896
dikhan <di.khan.r@gmail.com>
2020-06-11 16:47:07 -0700

    add gomplate template (html) and update main to render a simple terraformProviderDocumentation

--- utils/terraform_docs_generator/main.go
+++ utils/terraform_docs_generator/main.go
@@ -1,91 +1,104 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"github.com/dikhan/terraform-provider-openapi/utils/terraform_docs_generator/openapi"
-	"github.com/dikhan/terraform-provider-openapi/utils/terraform_docs_generator/openapi/printers"
-	"log"
-	"net/http"
-	"net/http/httptest"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"text/template"
 )
 
 func main() {
 
-	swaggerServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		swaggerYAMLTemplate := fmt.Sprintf(`swagger: "2.0"
-host: my-api.com
-schemes:
-- "http"
-security:
-  - api_auth: []
-securityDefinitions:
-  api_auth:
-    type: apiKey
-    name: Authorization
-    in: header
-x-terraform-provider-multiregion-fqdn: api.${region}.cloudflare.com
-x-terraform-provider-regions: sea, dub, rst, fra
-paths:
-  /cdns:
-    x-terraform-resource-name: cdn
-    post:
-      parameters:
-      - in: "body"
-        name: "body"
-        description: "Created CDN"
-        required: true
-        schema:
-          $ref: "#/definitions/ContentDeliveryNetworkV1"
-      - in: header
-        type: string
-        name: required_header_example
-        required: true
-      responses:
-        201:
-          description: "successful operation"
-          schema:
-            $ref: "#/definitions/ContentDeliveryNetworkV1"
-  /cdns/{id}:
-    get:
-      parameters:
-      - name: "id"
-        in: "path"
-        description: "The cdn id that needs to be fetched."
-        required: true
-        type: "string"
-      responses:
-        200:
-          description: "successful operation"
-          schema:
-            $ref: "#/definitions/ContentDeliveryNetworkV1"
-definitions:
-  ContentDeliveryNetworkV1:
-    type: "object"
-    required:
-      - label
-    properties:
-      id:
-        type: "string"
-        readOnly: true
-        description: System generated identifier for the CDN
-      label:
-        type: "string"
-        description: Label to use for the CDN`)
-		w.Write([]byte(swaggerYAMLTemplate))
-	}))
+	//	swaggerServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//		swaggerYAMLTemplate := fmt.Sprintf(`swagger: "2.0"
+	//host: my-api.com
+	//schemes:
+	//- "http"
+	//security:
+	//  - api_auth: []
+	//securityDefinitions:
+	//  api_auth:
+	//    type: apiKey
+	//    name: Authorization
+	//    in: header
+	//x-terraform-provider-multiregion-fqdn: api.${region}.cloudflare.com
+	//x-terraform-provider-regions: sea, dub, rst, fra
+	//paths:
+	//  /cdns:
+	//    x-terraform-docs-resource-description: The vdc resource allows managing a vdc in a specific region defined by the "region" property value specified in the provider configuration.
+	//    x-terraform-resource-name: cdn
+	//    post:
+	//      parameters:
+	//      - in: "body"
+	//        name: "body"
+	//        description: "Created CDN"
+	//        required: true
+	//        schema:
+	//          $ref: "#/definitions/ContentDeliveryNetworkV1"
+	//      - in: header
+	//        type: string
+	//        name: required_header_example
+	//        required: true
+	//      responses:
+	//        201:
+	//          description: "successful operation"
+	//          schema:
+	//            $ref: "#/definitions/ContentDeliveryNetworkV1"
+	//  /cdns/{id}:
+	//    get:
+	//      parameters:
+	//      - name: "id"
+	//        in: "path"
+	//        description: "The cdn id that needs to be fetched."
+	//        required: true
+	//        type: "string"
+	//      responses:
+	//        200:
+	//          description: "successful operation"
+	//          schema:
+	//            $ref: "#/definitions/ContentDeliveryNetworkV1"
+	//definitions:
+	//  ContentDeliveryNetworkV1:
+	//    type: "object"
+	//    required:
+	//      - label
+	//    properties:
+	//      id:
+	//        type: "string"
+	//        readOnly: true
+	//        description: System generated identifier for the CDN
+	//      label:
+	//        type: "string"
+	//        description: Label to use for the CDN`)
+	//		w.Write([]byte(swaggerYAMLTemplate))
+	//	}))
+	//
+	//	//var buf bytes.Buffer
+	//	//log.SetOutput(&buf)
+	//	//
+	//	//terraformDocGenerator := openapi.TerraformProviderDocGenerator{
+	//	//	OpenAPIDocURL: swaggerServer.URL,
+	//	//	Printer:       &printers.MarkdownPrinter{},
+	//	//	ProviderName:  "cloudflare", // TODO: add support for extension x-terraform-docs-provider-name
+	//	//}
+	//	//
+	//	//err := terraformDocGenerator.GenerateDocumentation()
+	//	//if err != nil {
+	//	//	log.Fatal(err)
+	//	//}
 
-	var buf bytes.Buffer
-	log.SetOutput(&buf)
+	absPath, _ := filepath.Abs("./utils/terraform_docs_generator/openapi/templates/html.tpl")
+	b, _ := ioutil.ReadFile(absPath)
 
-	terraformDocGenerator := openapi.TerraformProviderDocGenerator{
-		OpenAPIDocURL: swaggerServer.URL,
-		Printer:       &printers.MarkdownPrinter{},
-		ProviderName:  "cloudflare", // TODO: add support for extension x-terraform-docs-provider-name
-	}
-
-	err := terraformDocGenerator.GenerateDocumentation()
+	terraformProviderDocumentation := openapi.TerraformProviderDocumentation{ProviderName: "openapi"}
+	tmpl, err := template.New("TerraformProviderDocumentation").Parse(string(b))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+	err = tmpl.Execute(os.Stdout, terraformProviderDocumentation)
+	if err != nil {
+		panic(err)
+	}
+
 }
---
+++ utils/terraform_docs_generator/openapi/templates/html.tpl
@@ -0,0 +1,6 @@
+<p dir="ltr">
+  This guide lists the configuration for Streamline Terraform '{{.ProviderName}}' provider
+  resources that can be managed using
+  <a href="/hc/en-us/articles/360025293571" target="_self">Terraform v0.12</a>.&nbsp;
+</p>
+</p>
