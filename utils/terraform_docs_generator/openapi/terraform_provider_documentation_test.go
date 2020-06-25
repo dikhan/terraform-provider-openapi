d001cf0577d10fc3de8749a63532a597642f7f6f
dikhan <di.khan.r@gmail.com>
2020-06-25 14:57:15 -0700

    add coverage to ContainsComputedSubProperties

--- utils/terraform_docs_generator/openapi/terraform_provider_documentation_test.go
+++ utils/terraform_docs_generator/openapi/terraform_provider_documentation_test.go
@@ -31,3 +31,50 @@ func TestResource_BuildImportIDsExample(t *testing.T) {
 		assert.Equal(t, tc.expectedImportIDs, result)
 	}
 }
+
+func TestProperty_ContainsComputedSubProperties(t *testing.T) {
+	testCases := []struct {
+		name           string
+		property       Property
+		expectedResult bool
+	}{
+		{
+			name: "property does not have schema",
+			property: Property{
+				Name:   "some primitive property",
+				Schema: nil,
+			},
+			expectedResult: false,
+		},
+		{
+			name: "property does have a schema",
+			property: Property{
+				Name: "some property with schema (eg: object or array of objects) containing computed props",
+				Schema: []Property{
+					{
+						Name:     "subProperty",
+						Computed: true,
+					},
+				},
+			},
+			expectedResult: true,
+		},
+		{
+			name: "property does have a schema",
+			property: Property{
+				Name: "some property with schema (eg: object or array of objects) with no computed props",
+				Schema: []Property{
+					{
+						Name:     "subProperty",
+						Computed: false,
+					},
+				},
+			},
+			expectedResult: false,
+		},
+	}
+	for _, tc := range testCases {
+		result := tc.property.ContainsComputedSubProperties()
+		assert.Equal(t, tc.expectedResult, result)
+	}
+}
