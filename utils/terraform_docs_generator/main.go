de91b52dfea541481520656bbab2704637eaa9b6
dikhan <di.khan.r@gmail.com>
2020-06-30 12:40:38 -0700

    fix compilation error

--- utils/terraform_docs_generator/main.go
+++ utils/terraform_docs_generator/main.go
@@ -12,7 +12,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	f, err := os.Create("./utils/terraform_docs_generator/openapi/templates/zendesk_output.html")
+	f, err := os.Create("./utils/terraform_docs_generator/openapi/provider_documentation.html")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,7 +21,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = d.RenderZendeskHTML(f)
+	err = d.RenderHTML(f)
 	if err != nil {
 		log.Fatal(err)
 	}
